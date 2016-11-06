39c11fc8eb44c051f0fc27c9945df9e9ade18240
hayudi <hayudi@outlook.co.id>
2016-11-06 22:48:09 +0700

    Update ValidationErrorsMiddleware.php
    delete error on
    app\Middleware\ValidationErrorsMiddleware.php on line 13

--- app/Middleware/ValidationErrorsMiddleware.php
+++ app/Middleware/ValidationErrorsMiddleware.php
@@ -9,9 +9,11 @@
 		
 			
 		public function __invoke($request, $response, $next) {
-
-			$this->container->view->getEnvironment()->addGlobal('errors', $_SESSION['errors']);
-			unset($_SESSION['errors']);
+			
+			if(isset($_SESSION['errors'])) {
+				$this->container->view->getEnvironment()->addGlobal('errors', $_SESSION['errors']);
+				unset($_SESSION['errors']);
+			}
 
 
 			$response = $next($request, $response);
